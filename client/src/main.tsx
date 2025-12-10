3ff12d318b88e3704150a9fbfe93660a5fc061a2
RYthaGOD <RYthaGOD@users.noreply.github.com>
2025-12-10 18:01:28 +0800

    fix: add browser polyfills for Buffer and process to fix blank page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,10 @@
+// Polyfills for browser environment
+import { Buffer } from "buffer";
+// @ts-ignore
+window.Buffer = Buffer;
+// @ts-ignore
+window.process = window.process || { env: {} };
+
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
