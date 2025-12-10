2b380f34714b7a448393691b4d405c0d33dc68af
RYthaGOD <RYthaGOD@users.noreply.github.com>
2025-12-10 19:55:58 +0800

    fix: make browser polyfills robust

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -1,7 +1,12 @@
 import { Buffer } from "buffer";
 
-// @ts-ignore
-window.Buffer = Buffer;
-// @ts-ignore
-window.process = window.process || { env: {} };
-globalThis.Buffer = Buffer;
+if (typeof window !== "undefined") {
+    window.Buffer = Buffer;
+    window.process = window.process || { env: {} } as any;
+    (window as any).global = window;
+}
+
+if (typeof globalThis !== "undefined") {
+    globalThis.Buffer = Buffer;
+    globalThis.process = globalThis.process || { env: {} } as any;
+}
